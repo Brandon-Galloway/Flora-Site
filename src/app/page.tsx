413bab2c6bf6730b94b33a4631cddd9e90514b01
Brandon Galloway <brandon.w.galloway@gmail.com>
2023-12-15 01:18:57 -0600

    Added DaisyUI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ export default async function Home() {
   const readings = await fetchSensorData(targetDevice,{
       hours: 4
   });
-  const nums = [1,2,3]
   const device = (await fetchDevices(targetDevice))?.at(0);
   return (
       <div>
@@ -14,9 +13,36 @@ export default async function Home() {
               <p>Device: {device?.Nickname} [{device?.Location?.LocationName}]</p>
           </div>
           <div>
-          {readings?.map((reading, index) => (
-            <p key={index}>[{reading?.Timestamp}]: Air Temp:{reading?.AirTemperature}</p>
-          ))}
+          </div>
+          {/* Test Table */}
+          <div className="overflow-x-auto">
+            <table className="table table-xs">
+              <thead>
+                <tr>
+                  <th></th> 
+                  <th>Humidity</th> 
+                  <th>Air Temp</th> 
+                  <th>Soil Temp</th> 
+                  <th>Soil Moisture</th> 
+                  <th>Visible Light</th> 
+                  <th>Infrared Light</th>
+                </tr>
+              </thead> 
+              <tbody>
+              {readings?.map((reading, index) => (
+                // <p key={index}>[{reading?.Timestamp}]: Air Temp:{reading?.AirTemperature}</p>
+                <tr key={index}>
+                  <th>{index}</th> 
+                  <td>{reading?.Humidity}</td> 
+                  <td>{reading?.AirTemperature}</td> 
+                  <td>{reading?.SoilTemperature}</td> 
+                  <td>{reading?.SoilMoisture}</td> 
+                  <td>{reading?.VisibleLight}</td>
+                  <td>{reading?.InfraredLight}</td>
+                </tr>
+              ))}
+              </tbody> 
+            </table>
           </div>
       </div>
   )
