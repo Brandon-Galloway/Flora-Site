211cc68609e918892cc2595be40c4da8b5bb48e2
Brandon Galloway <brandon.w.galloway@gmail.com>
2024-01-16 17:54:57 -0600

    Added dark/light themes

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,22 +1,38 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import DarkModeSelector from './darkModeSelector';
+
 const Navbar = () => {
+
+    const navItems = [
+        { name: 'Overview', path: '/overview'},
+        { name: 'Dashboard', path: '/dashboard'}
+    ]
+
     return (
         <div className="navbar bg-base-100">
             <div className="navbar-start">
                 <div className="dropdown">
                 <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                    {/* <Image src="/test.svg" alt="Dropdown Icon" width={20} height={20}/> */}
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                 </div>
                 <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                    <li><a>Status</a></li>
-                    <li><a>Dashboard</a></li>
+                    {navItems.map((item, index) => (
+                        <li key={index}><Link href={item.path}>{item.name}</Link></li>
+                    ))} 
                 </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">Flora</a>
+                <Link href="/" className="btn btn-ghost text-xl">
+                    Flora
+                </Link>
             </div>
             <div className="navbar-end hidden lg:flex">
+                <DarkModeSelector className='menu menu-horizontal px-1' />
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Status</a></li>
-                    <li><a>Dashboard</a></li>
+                    {navItems.map((item, index) => (
+                        <li key={index}><Link href={item.path} className='text-xl'>{item.name}</Link></li>
+                    ))} 
                 </ul>
             </div>
         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,29 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
   plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["[data-theme=light]"],
+          "primary": "#7b4d3d",
+          "secondary": "#5c824b",
+          "accent": "#e4c487",
+          "neutral": "#888888",
+          "base-100": "#ffffff"
+        },
+        dark: {
+          ...require("daisyui/src/theming/themes")["[data-theme=dark]"],
+          "primary": "#ffffff",
+          "secondary": "#a0a0a0",
+          "accent": "#e4c487",
+          "neutral": "#666666",
+          "base-100": "#1a1a1a"
+        }
+      },
+    ],
+  },
 }
 export default config
+
